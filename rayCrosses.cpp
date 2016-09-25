318ca7961831ed05efc76a7fdb01d8693c22450c
rstinejr <bob@waltonstine.net>
2016-09-25 14:59:18 -0400

    Add method to compute polar cosine.

--- rayCrosses.cpp
+++ rayCrosses.cpp
@@ -4,12 +4,34 @@
 *
 * Avoid using trig library functions.
 */
+#include <cmath>
 #include <iostream>
 #include <utility>
 
 using namespace std;
 
 typedef pair<long, long> Point;
+
+/*
+* Law of cosines: a**2 = b**2 + c**2 - 2bc(cos(alpha))
+*
+* For polar cosine, segment c is a ray to the right of length one. 
+*
+*  So we have: cos(alpha) = (a**2 - b**2 - 1)/(-2ab)
+*
+*/
+double polarCosine(const Point& origin, const Point& p2)
+{
+   Point synth = make_pair(1, 0);
+   Point rmt   = make_pair(p2.first - origin.first, p2.second - origin.second);
+
+   long aSquared = (rmt.second * rmt.second) + (rmt.first - 1) * (rmt.first - 1);
+   long bSquared = (rmt.second * rmt.second) + (rmt.first * rmt.first);
+   double b        = sqrt((double) bSquared);
+
+
+   return -1.0 * ((double)(aSquared - bSquared - 1)) /(2.0 * b); 
+}
 
 bool rightRayCrosses(const Point& p, const Point& r1, const Point& r2)
 {
@@ -40,6 +62,13 @@ bool rightRayCrosses(const Point& p, const Point& r1, const Point& r2)
 
 int main(int argc, char *argv[])
 {
+    Point orig = make_pair(2, 2);
+    Point rmt  = make_pair(3, 3);
+
+    double polarCos = polarCosine(orig, rmt);
+
+    cout << "Polar cosine, (2,2) - (3,3): " << polarCos << endl;
+
     Point p0 = make_pair(5, 7);
 
     Point rl0 = make_pair(2, 11);
